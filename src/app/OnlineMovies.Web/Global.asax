05be2586f076616e7fc8c177387a6c4062196fef
chowd <chowdam.net@gmail.com>
2017-06-16 14:45:51 -0400

    Add project files.

---
+++ src/app/OnlineMovies.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineMovies.Web.MvcApplication" Language="C#" %>
---
+++ src/app/OnlineMovies.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineMovies.Web.Startup))]
+namespace OnlineMovies.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
